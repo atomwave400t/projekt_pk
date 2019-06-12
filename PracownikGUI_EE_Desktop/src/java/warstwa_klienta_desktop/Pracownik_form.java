1b9ba25489d1acc2a5b0699b3eccdceb82d985e7
root <root@DESKTOP-SDJGD18.localdomain>
2019-06-12 19:44:52 +0200

    Zmiana atrybutow wg Issue #1 - Pracownik_form.java

--- PracownikGUI_EE_Desktop/src/java/warstwa_klienta_desktop/Pracownik_form.java
+++ PracownikGUI_EE_Desktop/src/java/warstwa_klienta_desktop/Pracownik_form.java
@@ -23,41 +23,47 @@ import warstwa_biznesowa.dto.Pracownik_dto;
  * @author Kruczkiewicz
  */
 public class Pracownik_form extends JPanel implements ActionListener  {
-    JLabel lnazwa = new JLabel("Nazwa"); 		//utworzenie etykiety pola do wprowadzania nazwy pracownika
-    JTextField nazwa = new JTextField(15);		// utworzenie pola wejsciowego do wprowadzania nazwy pracownika
-    JLabel lcena = new JLabel("Cena");		 //utworzenie etykiety pola do wprowadzania ceny pracownika
-    JTextField cena = new JTextField(15);		 // utworzenie pola wejsciowego do wprowadzania ceny pracownika
-    JLabel lpromocja = new JLabel("Promocja");	 //utworzenie etykiety pola do wprowadzania promocji pracownika
-    JTextField promocja = new JTextField(15);		 // utworzenie pola wejsciowego do wprowadzania promocji pracownika
-    JLabel ldata = new JLabel("Data");		 //utworzenie etykiety pola do wprowadzania daty produkcji pracownika
-    JTextField data = new JTextField(15);		 // utworzenie pola wejsciowego do wprowadzania daty produkcji pracownika
-    JLabel lproducent = new JLabel("Producent");		 //utworzenie etykiety pola do wprowadzania producenta pracownika
-    JTextField producent = new JTextField(15);
-    JLabel ldata_przydatnosci = new JLabel("Data przydatnosci");		 //utworzenie etykiety pola do wprowadzania daty przydatnosci pracownika
-    JTextField data_przydatnosci = new JTextField(15);
-    JLabel lilosc = new JLabel("Ilosc");		 //utworzenie etykiety pola do wprowadzania ilosci pracownika
-    JTextField ilosc = new JTextField(15);    
+    //utworzenie obiektow odpowiednio etykiet i pól do wprowadzania danych
+    JLabel limie = new JLabel("Imie");
+    JTextField imie = new JTextField(15);
+    JLabel lnazwisko = new JLabel("Nazwisko");
+    JTextField nazwisko = new JTextField(15);
+    JLabel lnumerTelefonu = new JLabel("Numer Telefonu");
+    JTextField numerTelefonu = new JTextField(15);
+    JLabel ladres = new JLabel("Adres");
+    JTextField adres = new JTextField(15);
+    JLabel lmiasto = new JLabel("Miasto");
+    JTextField miasto = new JTextField(15);
+    JLabel lstanowisko = new JLabel("Stanowisko");
+    JTextField stanowisko = new JTextField(15);
+    JLabel lpensja = new JLabel("Pensja");
+    JTextField pensja = new JTextField(15);
+    JLabel ldataRozpoczeciaPracy = new JLabel("Data rozp. pracy");
+    JTextField dataRozpoczeciaPracy = new JTextField(15);    
     JButton dodaj_pracownik = new JButton("Dodaj pracownik");	//utworzenie przycisku do wywołania akcji dodania pracownika w aplikacji 
  
 public void init() {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));  	//dodanie sposobu rozmieszczania elementów formularza
-        add(lnazwa);			//dodanie etykiety nazwy do obiektu typu JPanel
-        add(nazwa);			//dodanie pola do wprowadzania nazwy do obiektu typu JPanel
-        add(lcena);			 //dodanie etykiety ceny do obiektu typu JPanel
-        add(cena);			 //dodanie pola do wprowadzania cenu do obiektu typu JPanel
-        add(lpromocja);			 //dodanie etykiety promocji do obiektu typu JPanel
-        add(promocja);			 //dodanie pola do wprowadzania promocji do obiektu typu JPanel
-        add(ldata);			 //dodanie etykiety daty do obiektu typu JPanel
-        add(data);			 //dodanie pola do wprowadzania daty do obiektu typu JPanel
-        add(lproducent);
-        add(producent);
-        add(ldata_przydatnosci);
-        add(data_przydatnosci);
-        add(lilosc);
-        add(ilosc);
+        //dodanie powyżej utworzonych elementów do JPanelu
+        add(limie);
+        add(imie);
+        add(lnazwisko);
+        add(nazwisko);
+        add(lnumerTelefonu);
+        add(numerTelefonu);
+        add(ladres);
+        add(adres);
+        add(lmiasto);
+        add(miasto);
+        add(lstanowisko);
+        add(stanowisko);
+        add(lpensja);
+        add(pensja);
+        add(ldataRozpoczeciaPracy);
+        add(dataRozpoczeciaPracy);
         dodaj_pracownik.addActionListener(this);	//przycisk uruchamiający zdarzenie po kliknieciu
 		                      	 	//obslugiwany przez obiekt typu Pracownik_form za pomoca metody actionPerformed
-        add(dodaj_pracownik);			//dodanie przycisku do obiektu typu JPanel
+        add(dodaj_pracownik);
     }
 
   public String content_validate(JTextField val, int typ) {//walidacja danych 
@@ -80,43 +86,29 @@ public void init() {
         return s;
     }
  public String[] form_pracownik() {
-        if (content_validate(nazwa, 0) == null) 			//walidacja danych nazwy jako lancucha
-            return null;  
-        if (content_validate(cena, 1) == null) 			//walidacja danych ceny jako liczby
+        if (content_validate(imie, 0) == null) 			//walidacja danych nazwy jako lancucha
             return null;
-        if (content_validate(promocja, 1) == null) 		//walidacja danych ceny jako liczby
+        if (content_validate(nazwisko, 0) == null) 			//walidacja danych nazwy jako lancucha
             return null;
-        if (content_validate(producent, 0) == null) 			//walidacja danych producenta jako lancucha
-            return null;  
-        if (content_validate(ilosc, 1) == null) 			//walidacja danych ilosci jako lancucha
+        if (content_validate(numerTelefonu, 0) == null) 			//walidacja danych nazwy jako lancucha
             return null;
-        String dane[] = {(String) nazwa.getText(), (String) cena.getText(), (String) promocja.getText(), (String) producent.getText(), (String) ilosc.getText()};	//tablica z danymi pracownika
+        if (content_validate(adres, 0) == null) 			//walidacja danych nazwy jako lancucha
+            return null; 
+        if (content_validate(miasto, 0) == null) 			//walidacja danych nazwy jako lancucha
+            return null;
+        if (content_validate(stanowisko, 0) == null) 			//walidacja danych nazwy jako lancucha
+            return null;
+        if (content_validate(pensja, 1) == null) 			//walidacja danych nazwy jako lancucha
+            return null;
+        String dane[] = {(String) imie.getText(), (String) nazwisko.getText(), (String) numerTelefonu.getText(), (String) adres.getText(), (String) miasto.getText(), (String) stanowisko.getText(), (String) pensja.getText()};	//tablica z danymi pracownika
         return dane;
     }
 
     public Date data() {
-        if (content_validate(data, 0) == null)			 //walidacja danych daty jako lancucha
+        if (content_validate(dataRozpoczeciaPracy, 0) == null)			 //walidacja danych daty jako lancucha
             return null;
         int rok, miesiac, dzien;
-        String data1 = data.getText();
-        try {
-            String[] data2 = data1.split("-");	//podzial lancucha daty np 12-12-2016 na tablicę lancuchow, reprezentujacych elementy daty
-            rok = Integer.parseInt(data2[2]);
-            miesiac = Integer.parseInt(data2[1]);
-            dzien = Integer.parseInt(data2[0]);
-        } catch (PatternSyntaxException | NumberFormatException | ArrayIndexOutOfBoundsException e) {        //kontrola poprawności 	 	JOptionPane.showMessageDialog(this, "Blad formatu daty");		         //formatu daty
-                          return null;
-        }
-        GregorianCalendar gc = new GregorianCalendar();		//pomocnicza klasa do utworzenia daty 
-        gc.set(rok, miesiac - 1, dzien);			//tworzenie daty
-        return gc.getTime();				//pobranie daty jako obiektu typu Date
-    }
-    
-    public Date data_przydatnosci() {
-        if (content_validate(data_przydatnosci, 0) == null)			 //walidacja danych daty jako lancucha
-            return null;
-        int rok, miesiac, dzien;
-        String data1 = data_przydatnosci.getText();
+        String data1 = dataRozpoczeciaPracy.getText();
         try {
             String[] data2 = data1.split("-");	//podzial lancucha daty np 12-12-2016 na tablicę lancuchow, reprezentujacych elementy daty
             rok = Integer.parseInt(data2[2]);
@@ -140,11 +132,7 @@ public void init() {
         if (data_ == null) {
             return;
         }
-        Date data_przydatnosci_ = data_przydatnosci();			//utworzenie daty przydatnosci
-        if (data_przydatnosci_ == null) {
-            return;
-        }
-        Pracownik_dto pracownik = new Pracownik_dto(dane, data_, data_przydatnosci_);
+        Pracownik_dto pracownik = new Pracownik_dto(dane, data_);
         GUI_main.getFacade().utworz_pracownika(pracownik);	// wywołanie metody logiki biznesowej tworzacej obiekt typu Pracownik1
     }
 }
